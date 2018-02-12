1e80ff41ecfe921e5042b21c8a35d0b1c66e977c
IanMFrost <ianmfrost0@gmail.com>
2018-02-12 12:48:26 +0100

    Oprettet klassen Student med properties

---
+++ SoapServiceOpgave/SoapServiceOpgave/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoapServiceOpgave
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+        public string Room { get; set; }
+        public int Grade { get; set; }
+       
+    }
+}
